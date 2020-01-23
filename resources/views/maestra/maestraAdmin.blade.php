745f32023af53d51627d3fdecd0d5ea0e9d9ff42
clase <5amariflo@gmail.com>
2020-01-23 11:03:42 +0100

    modificando master

--- resources/views/maestra/maestraAdmin.blade.php
+++ resources/views/maestra/maestraAdmin.blade.php
@@ -16,51 +16,79 @@
         <link rel="stylesheet" type="text/css" href="{{asset ('css/css_general.css')}}" media="screen" />
         <script src="jquery-3.4.1.min.js"></script>
     </head>
-    
+
     <body>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <header class="nav">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="">
+                                    <img src="" alt="logotipo">
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Logo</a>
+                            </li>
+<!--                            <li class="nav-item">
+                                <a class="nav-link active" href="">Cambiar de rol</a>
+                            </li>-->
+                            <li class="nav-item justify-content-end">
+                                <form name="perfil" action="perfil"  method="post">
+                                    {{ csrf_field() }}  
+                                    <div class="form-group">
+                                        <button type="submit" id="perfil" name="perfil" value=""></button>
+                                    </div>
+                                </form>
+                            </li>
+                            <li class="nav-item justify-content-end">
+                                <form name="cerrarSesion" action="cerrarSesion"  method="post">
+                                    {{ csrf_field() }}  
+                                    <div class="form-group">
+                                        <button type="submit" id="cerrarSesion" name="cerrarSesion" value=""></button>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </header>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <nav  class="nav">
+                        <ul class="nav">
 
-        <nav  class="nav">
-            <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Home</a>
+                            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
-                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Exportar Documentos</a>
+                            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="">Exportar Documentos</a>
-                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Gestionar Usuarios</a>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="">Gestionar Tutores</a>
+                                    <a class="dropdown-item" href="">Gestionar Alumnos</a>
+                                    <a class="dropdown-item" href="">Gestionar Usuarios</a>
+                                </div>
+                            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link" href="">Gestionar Usuarios</a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="">Gestionar Tutores</a>
-                        <a class="dropdown-item" href="">Gestionar Alumnos</a>
-                        <a class="dropdown-item" href="">Gestionar Usuarios</a>
-                    </div>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="">Importar Tutores</a>
-                </li>
-                
-                <li class="nav-item justify-content-end">
-                    <form name="cerrarSesion" action="cerrarSesion"  method="post">
-                        {{ csrf_field() }}  
-                        <div class="form-group">
-                            <button type="submit" id="cerrarSesion" name="cerrarSesion" value=""></button>
-                        </div>
-                    </form>
-                </li>
-
-            </ul>
-
-        </nav>
-
-        @yield('contenido')
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Importar Tutores</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            @yield('contenido')
+        </div>
     </body>
 </html>
--- resources/views/maestra/maestraAlumno.blade.php
+++ resources/views/maestra/maestraAlumno.blade.php
@@ -16,41 +16,70 @@
         <link rel="stylesheet" type="text/css" href="{{asset ('css/css_general.css')}}" media="screen" />
         <script src="jquery-3.4.1.min.js"></script>
     </head>
-    
+
     <body>
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        
-        <nav  class="nav">
-            <ul class="nav">
-                
-                <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
-                </li>
-                
-                <li class="nav-item">
-                    <a class="nav-link" href="">Crear Gasto</a>
-                </li>
-                
-                <li class="nav-item">
-                    <a class="nav-link" href="">Gestionar Gastos</a>
-                </li>
-                
-                <li class="nav-item justify-content-end">
-                    <form name="cerrarSesion" action="cerrarSesion"  method="post">
-                        {{ csrf_field() }}  
-                        <div class="form-group">
-                            <button type="submit" id="cerrarSesion" name="cerrarSesion" value=""></button>
-                        </div>
-                    </form>
-                </li>
-                
-            </ul>
-        </nav>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <header class="nav">
+                        <ul class="nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="">
+                                    <img src="" alt="logotipo">
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Logo</a>
+                            </li>
+                            <!--                            <li class="nav-item">
+                                                            <a class="nav-link active" href="">Cambiar de rol</a>
+                                                        </li>-->
+                            <li class="nav-item justify-content-end">
+                                <form name="perfil" action="perfil"  method="post">
+                                    {{ csrf_field() }}  
+                                    <div class="form-group">
+                                        <button type="submit" id="perfil" name="perfil" value=""></button>
+                                    </div>
+                                </form>
+                            </li>
+                            <li class="nav-item justify-content-end">
+                                <form name="cerrarSesion" action="cerrarSesion"  method="post">
+                                    {{ csrf_field() }}  
+                                    <div class="form-group">
+                                        <button type="submit" id="cerrarSesion" name="cerrarSesion" value=""></button>
+                                    </div>
+                                </form>
+                            </li>
+                        </ul>
+                    </header>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <nav  class="nav">
+                        <ul class="nav">
 
-        @yield('contenido')
+                            <li class="nav-item">
+                                <a class="nav-link active" href="">Home</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Crear Gasto</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Gestionar Gastos</a>
+                            </li>                
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            @yield('contenido')
+        </div>
     </body>
 </html>
