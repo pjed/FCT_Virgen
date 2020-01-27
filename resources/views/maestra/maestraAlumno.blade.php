dc3cb96a861f5725fc4c5990bdbcced1f2dd3ab9
clase <5amariflo@gmail.com>
2020-01-27 12:15:02 +0100

    añadido vistas 	bienvenidaAd 	bienvenidaAl 	bienvenidaT 	gestionarAlumnos 	gestionarTutores 	importarAlumnos 	importarTutores 	cambiarRol 	extraerDocA 	extraerDocT 	modificarT 	consultarGastoAlumno modificado vistas 	crearGasto 	inicioSesion 	maestraAdmin 	maestraAlumno 	maestraGeneral 	maestraTutor 	consultarGastosCurso Modificado	web 	añadido los enlaces de los menus de las bistas maestras, 		con method get

--- resources/views/maestra/maestraAlumno.blade.php
+++ resources/views/maestra/maestraAlumno.blade.php
@@ -13,8 +13,9 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
         <!-- Course CSS -->
-        <link rel="stylesheet" type="text/css" href="{{asset ('css/css_general.css')}}" media="screen" />
-        <script src="jquery-3.4.1.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="{{asset ('css/css_general.css')}}" media="screen" />        
+        <link rel="stylesheet" type="text/css" href="{{asset ('css/css_gestionar.css')}}" media="screen" />
+        <script src="{{asset ('js/jquery-3.3.1.min.js')}}"></script>
     </head>
 
     <body>
@@ -23,88 +24,94 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-        <div class="container">
-            <div class="row">
+        <header class="fixed-top">  
+           <div class="row">
                 <div class="col">
-                    <header class="nav">
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="">
-                                    <img src="" alt="logotipo">
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active" href="">Logo</a>
-                            </li>
-                            <!--                            <li class="nav-item">
-                                                            <a class="nav-link active" href="">Cambiar de rol</a>
-                                                        </li>-->
-                            <li class="nav-item justify-content-end">
-                                <form name="perfil" action="perfil"  method="post">
-                                    {{ csrf_field() }}  
-                                    <div class="form-group">
-                                        <button type="submit" id="perfil" name="perfil" value=""></button>
-                                    </div>
-                                </form>
-                            </li>
-                            <li class="nav-item justify-content-end">
-                                <form name="cerrarSesion" action="cerrarSesion"  method="post">
-                                    {{ csrf_field() }}  
-                                    <div class="form-group">
-                                        <button type="submit" id="cerrarSesion" name="cerrarSesion" value=""></button>
-                                    </div>
-                                </form>
-                            </li>
-                        </ul>
-                    </header>
+                    <ul class="list-group  list-group-flush">
+                        <div class="row">
+                            <div class="col-3">
+                                <li class="list-group-item">
+                                    <a href="#">
+                                        <img id="logotipo" src="{{asset ('imagenes/logo.png')}}" alt="logotipo">
+                                    </a>
+                                </li>
+                            </div>
+                            <div class="col-3">
+                                <li class="list-group-item">
+                                    <a href="#">Nombre de la aplicacion</a>
+                                </li>
+                            </div>
+                            <div class="col-3">
+                                <li class="list-group-item">
+                                    <form name="perfil" action="perfil"  method="post">
+                                        {{ csrf_field() }}  
+                                        <div class="form-group">
+                                            <button type="submit" id="perfil" name="perfil" value=""></button>
+                                        </div>
+                                    </form>
+                                </li>
+                            </div>
+                            <div class="col-3">
+                                <li class="list-group-item">
+                                    <form name="cerrarSesion" action="cerrarSesion"  method="post">
+                                        {{ csrf_field() }}  
+                                        <div class="form-group">
+                                            <button type="submit" id="cerrarSesion" name="cerrarSesion" value=""></button>
+                                        </div>
+                                    </form>
+                                </li>
+                            </div>
+                        </div>
+                    </ul>
                 </div>
             </div>
             <div class="row">
                 <div class="col">
                     <nav  class="nav">
                         <ul class="nav">
-
                             <li class="nav-item">
-                                <a class="nav-link active" href="">Home</a>
+                                <a class="nav-link active" href="bienvenidaAl">Home</a>
                             </li>
-
                             <li class="nav-item">
-                                <a class="nav-link" href="">Crear Gasto</a>
+                                <a class="nav-link" href="crearGasto">Crear Gasto</a>
                             </li>
-
                             <li class="nav-item">
-                                <a class="nav-link" href="">Gestionar Gastos</a>
+                                <a class="nav-link" href="gestionarGastos">Gestionar Gastos</a>
                             </li>                
                         </ul>
                     </nav>
                 </div>
             </div>
+        </header>
+        <div class="container">
             @yield('contenido')
-            <div class="row">
-                <div class="col">
-                    <footer class="nav">
-                        <ul class="nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="">
-                                    <img src="" alt="logotipo instituto">
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    2 DAW 2019 - 2020
-                                </a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">
-                                    Marina Estefania Flores Fernandez
-                                    Pedro Javier Espinosa
-                                    Manuel Ruis Gonzalez
-                                </a>
-                            </li>
-                        </ul>
-                    </footer>
-                </div>
-            </div>
         </div>
+        <footer class="fixed-bottom">   
+            <ul class="list-group list-group-flush">
+                <div class="row">
+                    <div class="col-4">
+                        <li class="list-group-item">
+                            <a class="nav-link" href="http://www.cifpvirgendegracia.com/">
+                                <img id="logoInstituto" src="{{asset ('images/logoInstituto.png')}}" alt="logotipo instituto">
+                            </a>
+                        </li>
+                    </div>
+                    <div class="col-5">
+                        <li class="list-group-item">
+                            2 DAW 2019 - 2020
+                        </li>
+                    </div>
+                    <div class="col-3">
+                        <li class="list-group-item">
+                            Marina Estefania Flores Fernandez
+                            <br>
+                            Pedro Javier Espinosa
+                            <br>
+                            Manuel Ruis Gonzalez
+                        </li>
+                    </div>
+                </div>
+            </ul>
+        </footer>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+  |--------------------------------------------------------------------------
+  | Web Routes
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you can register web routes for your application. These
+  | routes are loaded by the RouteServiceProvider within a group which
+  | contains the "web" middleware group. Now create something great!
+  |
+ */
 
 Route::get('/', function () {
     return view('inicioSesion');
@@ -19,23 +19,79 @@ Route::get('/', function () {
 Route::post('inicioSesion', 'controladorGeneral@inicioSesion');
 Route::post('cerrarSesion', 'controladorGeneral@cerrarSesion');
 Route::post('olvidarPwd', 'controladorGeneral@olvidarPwd');
+
 //Route::group(['middleware' => ['general']], function() {
-    Route::get('inicioSesion', 'controladorGeneral@inicioSesion');
-    Route::get('cerrarSesion', 'controladorGeneral@cerrarSesion');    
-    Route::get('olvidarPwd', 'controladorGeneral@olvidarPwd');
+Route::get('inicioSesion', 'controladorGeneral@inicioSesion');
+Route::get('cerrarSesion', 'controladorGeneral@cerrarSesion');
+Route::get('olvidarPwd', 'controladorGeneral@olvidarPwd');
 //});
 
 //tutor
 //Route::group(['middleware' => ['tutor']], function() {
-    
+Route::get('bienvenidaT', function () {
+    return view('bienvenidaT');
+});
+Route::get('consultarAlumno', function () {
+    return view('consultarGastosAlumno');
+});
+Route::get('consultarCurso', function () {
+    return view('consultarGastosCurso');
+});
+Route::get('ModificarDoc', function () {
+    return view('modificarDoc');
+});
+Route::get('ExtraerDocT', function () {
+    return view('extraerDocT');
+});
+Route::get('importarAlumnos', function () {
+    return view('importarAlumnos');
+});
 //});
 
 //admin
 //Route::group(['middleware' => ['admin']], function() {
-    
+    Route::get('bienvenidaAd', function () {
+    return view('bienvenidaAd');
+});
+Route::get('extraerDocA', function () {
+    return view('extraerDocA');
+});
+Route::get('gestionarUsuarios', function () {
+    return view('gestionarUsuarios');
+});
+Route::get('gestionarAlumnos', function () {
+    return view('gestionarAlumnos');
+});
+Route::get('gestionarUsuarios', function () {
+    return view('gestionarUsuarios');
+});
+Route::get('gestionarTutores', function () {
+    return view('gestionarTutores');
+});
+Route::get('importarTutores', function () {
+    return view('importarTutores');
+});
+
 //});
 
 //alumno
 //Route::group(['middleware' => ['alumno']], function() {
-  
+  Route::get('bienvenidaAl', function () {
+    return view('bienvenidaAl');
+});
+Route::get('crearGasto', function () {
+    return view('crearGasto');
+});
+Route::get('gestionarGastos', function () {
+    return view('gestionarGastos');
+});
+
+//});
+
+
+//admin y tutor
+//Route::group(['middleware' => ['admin','tutor']], function() {
+Route::get('cambiarRol', function () {
+    return view('cambiarRol');
+});
 //});
