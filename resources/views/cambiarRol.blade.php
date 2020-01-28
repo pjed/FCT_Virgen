1848af3098a9ec1e50ce0ee6cbca3027db5c4a43
clase <5amariflo@gmail.com>
2020-01-28 14:28:56 +0100

    modificado cambiar rol 	se ven los botones para poder avanzar en la aplicacion

--- resources/views/cambiarRol.blade.php
+++ resources/views/cambiarRol.blade.php
@@ -10,36 +10,16 @@ Inicio de sesion
         <main class="mt-5 mb-5 "> 
             <div class="row justify-content-center">
                 <div class="col-sm col-md-2 col-lg">
-                    <h1>Iniciar Sesión</h1>
+                    <h1>Cambiar rol</h1>
                 </div>
             </div>
             <div class="row justify-content-center">
                 <div class="col-sm col-md-2 col-lg">       
-                    <form name="inicioSesion" action="inicioSesion" method="POST">
+                    <form name="cambiarRol" action="cambiarRol" method="POST">
                         {{ csrf_field() }}
                         <div class="form-group">
-                            <label for="email">
-                                Usuario:
-                                <input type="email"class="form-control form-control-sm form-control-md form-control-lg" id="usuario" name="usuario" placeholder="usuario@x.x" data-toggle="tooltip" data-placement="left" data-html="true" title="Introduca un correo"><br>
-                            </label>
-                        </div>
-                        <div class="form-group">
-                            <label for="pwd">
-                                Contraseña:
-                                <input type="password" class="form-control form-control-sm form-control-md form-control-lg" id="pwd" name="pwd" placeholder="Contraseña" data-toggle="tooltip" data-placement="left" data-html="true" title="Introduca una contraseña"><br>
-                            </label>
-                        </div>
-                        <div class="form-group">
-                            <a href="olvidarPwd">He olvidado la contraseña</a>
-                        </div>
-                        <div class="form-group">
-                            <label>
-                                <input type="checkbox" id="guardarSesion" name="guardarSesion"/>
-                                Recuérdame
-                            </label>
-                        </div>
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-primary" id="aceptar" name="aceptarIndex" value="Aceptar">
+                            <input type="submit" class="btn btn-primary" name="tutor" value="Tutor">
+                            <input type="submit" class="btn btn-primary" name="administrador" value="Administrador">
                         </div>
                     </form>
                 </div>
