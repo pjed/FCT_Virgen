fbabd0f207aa10ae2c8a99be502ddc19fc5b9004
clase <5amariflo@gmail.com>
2020-02-05 11:58:46 +0100

    modificado cambiar rol el titulo de la pestaña

--- resources/views/cambiarRol.blade.php
+++ resources/views/cambiarRol.blade.php
@@ -1,30 +1,22 @@
 @extends('maestra.maestraGeneral')
 
 @section('titulo') 
-Inicio de sesion
+Cambiar Rol
 @endsection
 
 @section('contenido') 
+<h1>Cambiar rol</h1>
 <div class="row justify-content-center">
     <div class="col-sm col-md-2 col-lg">
-        <main class="mt-5 mb-5 "> 
-            <div class="row justify-content-center">
-                <div class="col-sm col-md-2 col-lg">
-                    <h1>Cambiar rol</h1>
-                </div>
+
+        <form name="cambiarRol" action="cambiarRol" method="POST">
+            {{ csrf_field() }}
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" name="tutor" value="Tutor">
+                <input type="submit" class="btn btn-primary" name="administrador" value="Administrador">
             </div>
-            <div class="row justify-content-center">
-                <div class="col-sm col-md-2 col-lg">       
-                    <form name="cambiarRol" action="cambiarRol" method="POST">
-                        {{ csrf_field() }}
-                        <div class="form-group">
-                            <input type="submit" class="btn btn-primary" name="tutor" value="Tutor">
-                            <input type="submit" class="btn btn-primary" name="administrador" value="Administrador">
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </main>
+        </form>
+
     </div>
 </div>
 @endsection
