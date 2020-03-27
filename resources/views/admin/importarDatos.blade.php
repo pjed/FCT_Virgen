da285dd38efccf55c909e573355cf397e019dfc8
DarkS <DarkS@192.168.2.22>
2020-03-27 14:02:23 +0100

    Drag and drop importar datos

--- resources/views/admin/importarDatos.blade.php
+++ resources/views/admin/importarDatos.blade.php
@@ -1,7 +1,9 @@
 @extends('maestra.maestraAdmin')
-<script src="http://demo.itsolutionstuff.com/plugin/jquery.js"></script>
-<link rel="stylesheet" type="text/css" href="{{asset ('css/dropzone.css')}}"/>   
-<script src="{{asset ('js/dropzone.js')}}"></script>
+<meta name="_token" content="{{csrf_token()}}" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/min/dropzone.min.css">
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.4.0/dropzone.js"></script>
 @section('titulo') 
 Importar Datos
 @endsection
@@ -18,25 +20,74 @@ Importar Datos
         </nav>
     </nav>
 
-    <h2 class="text-center">Subir Archivos</h2>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                {!! Form::open([ 'route' => [ 'dropzone.store' ], 'files' => true, 'enctype' => 'multipart/form-data', 'class' => 'dropzone', 'id' => 'image-upload' ]) !!}
-                <div>
-                    <h4>Arrastrar los archivos para añadirlos a la subida</h4>
-                    <h5>Archivos permitidos unicamente .sql</h5>
+    <!--    <h2 class="text-center">Subir Archivos</h2>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    {!! Form::open([ 'route' => [ 'dropzone.store' ], 'files' => true, 'enctype' => 'multipart/form-data', 'class' => 'dropzone', 'id' => 'image-upload' ]) !!}
+                    <div>
+                        <h4>Arrastrar los archivos para añadirlos a la subida</h4>
+                        <h5>Archivos permitidos unicamente .sql</h5>
+                    </div>
+                    {!! Form::close() !!}
                 </div>
-                {!! Form::close() !!}
             </div>
         </div>
+        <script type="text/javascript">
+            Dropzone.options.imageUpload = {
+                maxFilesize: 5, //MB
+                acceptedFiles: ".sql",
+                addRemoveLinks: true,
+            };
+        </script>-->
+    <div class="container">
+
+        <h3>Laravel Dropzone Multiple Images Uploading - W3Adda</h3>
+        <form method="post" action="{{url('dropzone-image-upload')}}" enctype="multipart/form-data" 
+              class="dropzone" id="dropzone">
+            @csrf
+        </form>   
+        <script type="text/javascript">
+            Dropzone.options.dropzone =
+                    {
+                        maxFilesize: 12,
+                        renameFile: function (file) {
+                            var dt = new Date();
+                            var time = dt.getTime();
+                            return time + file.name;
+                        },
+                        acceptedFiles: ".sql",
+                        timeout: 5000,
+                        addRemoveLinks: true,
+                        removedfile: function (file)
+                        {
+                            var name = file.upload.filename;
+                            $.ajax({
+                                headers: {
+                                    'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                                },
+                                type: 'POST',
+                                url: '{{ url("uploads") }}',
+                                data: {filename: name},
+                                success: function (data) {
+                                    console.log("File deleted successfully!!");
+                                },
+                                error: function (e) {
+                                    console.log(e);
+                                }});
+                            var fileRef;
+                            return (fileRef = file.previewElement) != null ?
+                                    fileRef.parentNode.removeChild(file.previewElement) : void 0;
+                        },
+                        success: function (file, response)
+                        {
+                            console.log(response);
+                        },
+                        error: function (file, response)
+                        {
+                            return false;
+                        }
+                    };
+        </script>
     </div>
-    <script type="text/javascript">
-        Dropzone.options.imageUpload = {
-            maxFilesize: 5, //MB
-            acceptedFiles: ".sql",
-            addRemoveLinks: true,
-        };
-    </script>
-</div>
-@endsection
+    @endsection
--- routes/web.php
+++ routes/web.php
@@ -97,8 +97,11 @@ Route::group(['middleware' => ['admin']], function() {
     Route::get('importarDatos', function () {
         return view('admin/importarDatos');
     });
-    Route::get('dropzone', 'DropzoneController@dropzone');
-    Route::post('dropzone/store', 'DropzoneController@dropzoneStore')->name('dropzone.store');
+    Route::get('dropzone', 'DropzoneController@index')->name('dropzone.store');
+    Route::post('dropzone-image-upload', 'DropzoneController@store');
+    Route::post('dropzone-image-delete', 'DropzoneController@destroy');
+//    Route::get('dropzone', 'DropzoneController@dropzone');
+//    Route::post('dropzone/store', 'DropzoneController@dropzoneStore')->name('dropzone.store');
     Route::get('extraerDocA', function () {
         $l1 = Conexion::listaCursos();
         $datos = [
