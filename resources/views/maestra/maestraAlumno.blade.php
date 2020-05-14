2dae568ace9761bf13fcdb483588bcd424b9872b
FF <5amarinaflo@gmail.com>
2020-05-14 14:37:45 +0200

    -Añadido @yield('css') a las maestras para asi poder poner un css solo en un pagina

--- resources/views/maestra/maestraAlumno.blade.php
+++ resources/views/maestra/maestraAlumno.blade.php
@@ -18,6 +18,7 @@ $rol1 = session()->get('rol');
         <link rel="stylesheet" type="text/css" href="{{asset ('css/css_general.css')}}" media="screen" />        
         <link rel="stylesheet" type="text/css" href="{{asset ('css/bootstrap.min.css')}}" media="screen" />         
         <link rel="stylesheet" type="text/css" href="{{asset ('css/css_gestionar.css')}}" media="screen" />
+        @yield('css')
 
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
--- resources/views/maestra/maestraGeneral.blade.php
+++ resources/views/maestra/maestraGeneral.blade.php
@@ -14,6 +14,7 @@
         <!-- Course CSS -->
         <link rel="stylesheet" type="text/css" href="{{asset ('css/css_general.css')}}" media="screen" />    
         <link rel="stylesheet" type="text/css" href="{{asset ('css/bootstrap.min.css')}}" media="screen" /> 
+        @yield('css')
 
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
         <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
