401d8389bc68a5951b2c46be8aa120b737bc7063
FF <5amarinaflo@gmail.com>
2020-05-28 13:01:29 +0200

    - menu horizontal responvie en administrador solo añadido icono del menu hamburguesa y links para que aparezca dicho menu

--- resources/views/maestra/maestraAdmin.blade.php
+++ resources/views/maestra/maestraAdmin.blade.php
@@ -27,6 +27,11 @@ $rol1 = session()->get('rol');
         <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>-->
         <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>-->
 
+        
+        <!-- Font Awesome JS -->
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/solid.js" integrity="sha384-tzzSw1/Vo+0N5UhStP3bvwWPq+uvzCMfrN1fEFe+xBmv1C/AtVX5K0uZtmcHitFZ" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.0.13/js/fontawesome.js" integrity="sha384-6OIrr52G08NpOFSZdxxz1xdNSndlD4vdcf/q2myIUVO0VsqaGHJsB0RaBE01VTOY" crossorigin="anonymous"></script>
+
         <!-- Javascript -->
         <script src="{{asset ('js/jquery-3.2.1.slim.min.js')}}"></script>
         <script src="{{asset ('js/popper.min.js')}}"></script>
@@ -90,8 +95,8 @@ $rol1 = session()->get('rol');
                     <nav class="navbar navbar-default">
                         <div class="container-fluid">
                             <div class="navbar-header">
-                                <button class="navbar-toggler navbar-btn" type="button" id="sidebarCollapse" data-toggle="collapse" >
-                                    <span class="navbar-toggler-icon"></span>  
+                                <button class="btn btn-dark" type="button" id="sidebarCollapse" class="btn btn-info">
+                                    <i class="fas fa-align-justify"></i>
                                 </button>
                             </div>
                             <ul class="nav navbar-nav navbar-right">
